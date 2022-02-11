329ed11ed35c39d512b54539bfaebd6080f34dfc
Álvaro Fernández <avunotfound@gmail.com>
2022-02-11 09:30:33 +0100

    a

--- src/main/Producto.java
+++ src/main/Producto.java
@@ -5,6 +5,30 @@ public class Producto {
     String descripcion;
     float precio;
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
     public Producto(int codigo, String descripcion, float precio) {
         this.codigo = codigo;
         this.descripcion = descripcion;
